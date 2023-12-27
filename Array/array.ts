f6149ec1475386b6b0e26aa39ce8bba8d4b00b58
sugam <nextgenachieverss@gmail.com>
2023-12-27 22:00:40 +0530

    basic array

---
+++ Array/array.ts
@@ -0,0 +1,13 @@
+let rollNumber: number[]; 
+rollNumber = [101, 102, 103, 104, 105]; 
+
+let studentName: string[] = ["Rahul", "Raj", "Sonam", "Sumit", "Ranjan"];
+
+console.log("Roll Number: " + rollNumber + "\n");
+console.log("Student Name: " + studentName + "\n");
+
+const fruits: Array<string> = ["Apple", "Orange", "Banana", "Grapes", "Mango"];
+
+let employee: Array<string | number> = ["Rahul", 101, "Raj", 102, "Sonam", 103, "Sumit", 104, "Ranjan", 105];
+
+let employee2: (string | number)[] = ["Rahul", 101, "Raj", 102, "Sonam", 103, "Sumit", 104, "Ranjan", 105];
