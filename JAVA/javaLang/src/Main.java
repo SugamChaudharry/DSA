485f3dce905b0c614bdf2b3c8551fdf3d8eca4b2
sugam <nextgenachieverss@gmail.com>
2024-01-04 10:57:27 +0530

    java lang first commit

---
+++ JAVA/javaLang/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    // function name has to be main only
+    // public :- access modifier
+    // static :- no need to create object to call main function
+    // void :- return type that means main function will not return anything
+    // String[] args :- command line arguments are passed as array of strings
+    public static void main(String[] args) {
+
+        System.out.println(args[0]);
+    }
+}
