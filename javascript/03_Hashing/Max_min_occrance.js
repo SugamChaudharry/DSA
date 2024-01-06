69205c4a4d7a201193d463d1597cf86357e59ec9
sugam <nextgenachieverss@gmail.com>
2024-01-06 16:23:43 +0530

    FIX :::: Issue :::: IN :::: QUESTION

--- javascript/03_Hashing/Max_min_occrance.js
+++ javascript/03_Hashing/Max_min_occrance.js
@@ -1,8 +1,7 @@
-let arr = [10, 5, 10, 15, 10, 5, 5,16];
+let arr = [10, 5, 10, 15,  5, 5];
 let fq = [];
 arr.sort((a, b) => a - b);
 let max = arr[arr.length - 1];
-let arrr = max;
 for (let i = 0; i <= max; i++) {
   fq[i] = 0;
 }
@@ -10,31 +9,25 @@ arr.forEach((e) => {
   fq[e]++;
 });
 
-let i = 0;
-let f = 0;
 max = [0,0];
-let min = [0,0];
+arr = [0,Number.MAX_VALUE]; // re-using arr array as min array
 
 for (let i = 0; i < fq.length; i++) {
   if (fq[i] !== 0) {
+
     if (fq[i] > max[1]) {
-      max[1] = fq[i];
-      max[0] = i;
-      if(f===0){
-        min[1] = fq[i];
-        min[0] = i;
-        f = 1
-      }
+      max = [i , fq[i]]
     }else if (fq[i] === max[1]) {
       max[0] = [max[0],i];
     }
-    if(fq[i]<min[1]){
-         min[1] = fq[i];
-         min[0] = i;
-    }else if(fq === min[1]){
-        min[0] = [min[0], i];
+
+    if(fq[i] < arr[1]){
+      arr = [i , fq[i]]
+    }else if (fq[i] === arr[1]){
+      arr[0] = [arr[0] , i]
     }
   }
 }
 console.log(max);
-console.log(min);
+arr = arr[1] === Number.MAX_VALUE ? [0,0] : arr;
+console.log(arr);
