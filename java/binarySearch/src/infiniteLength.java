77ec8c949d3cba39ade9c676419af601ce72fb37
SugamChaudharry <nextgenachieverss@gmail.com>
2024-06-12 11:16:33 +0530

    completed BinarySearch questions

--- java/binarySearch/src/infiniteLength.java
+++ java/binarySearch/src/infiniteLength.java
@@ -1,6 +1,71 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class infiniteLength {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter size of arr : ");
+
+        int size = input.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter elements of arrays : ");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = input.nextInt();
+        }
+        boolean stop = false;
+
+        do {
+            System.out.println("what element you want to find : ");
+            int target = input.nextInt();
+
+            System.out.println(Arrays.toString(arr));
+            System.out.print(" ");
+
+            int ind = ans(arr,target);
+
+            char[] sb = {'^','|'};
+            for (int i = 0; i <= 2; i++) {
+                for (int j = 0; j <= ind; j++) {
+                    if (j == ind && i < 2) {
+                        System.out.print(sb[i]);
+                    }else {
+                        if (j == ind){
+                            System.out.print(ind);
+                        }else {
+                            if (i == 2) {
+                                System.out.print(j);
+                                System.out.print("  ");
+                            }else {
+                                System.out.print("   ");
+                            }
+                        }
+                    }
+
+                }
+                System.out.println();
+                System.out.print(" ");
+            }
+            System.out.print("Do you want to continue [y/n] : ");
+            char f = input.next().trim().charAt(0);
+            if (f != 'y'){
+                stop = true;
+            }
+        }while (!stop);
+    }
+
+    static  int ans(int[] arr, int target){
+        int start = 0;
+        int end = 1;
+
+        while (target > arr[end]){
+            int temp = end + 1;
+            end = end + (start + (end - start)/2)*2;
+            start = temp;
+        }
+        return binarySearch(arr , target, start, end);
     }
     static int binarySearch(int[] arr, int target, int start, int end ) {
 
---
+++ java/binarySearch/src/peakInMountain.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class peakInMountain {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter size of arr : ");
+
+        int size = input.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter elements of arrays : ");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
+        int ind = binarySearch(arr);
+        char[] sb = {'^','|'};
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= ind; j++) {
+                if (j == ind && i < 2) {
+                    System.out.print(sb[i]);
+                }else {
+                    if (j == ind){
+                        System.out.print(ind);
+                    }else {
+                        if (i == 2) {
+                            System.out.print(j);
+                            System.out.print("  ");
+                        }else {
+                            System.out.print("   ");
+                        }
+                    }
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    static int binarySearch(int[] arr ) {
+        int start = 0;
+        int end = arr.length-1;
+        int mid = start + (end - start) / 2;
+
+        while (start < end){
+
+            if (arr[mid + 1 ] > arr[mid]){
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+            mid = start + (end - start) / 2;
+
+        }
+
+        return mid;
+
+    }
+}
