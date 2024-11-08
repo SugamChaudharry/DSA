c2aee417e38980b281d2058bc6dd116f61867fda
SugamChaudharry <nextgenachieverss@gmail.com>
2024-11-08 06:15:52 +0530

    traversal methods

--- java/tree/src/Main.java
+++ java/tree/src/Main.java
@@ -7,6 +7,17 @@ public class Main {
         tree.populate(input);
         tree.display();
         tree.prettyDisplay();
+        BinarySearchTree t = new BinarySearchTree();
+        t.insert(6);
+        t.insert(3);
+        t.insert(1);
+        t.insert(4);
+        t.insert(8);
+        t.insert(10);
+        t.insert(7);
+        t.insert(9);
+        t.display();
+        t.prettyDisplay();
     }
 }
 
