519a586b83c720d28676eebddf4a78df825d39c2
SugamChaudharry <nextgenachieverss@gmail.com>
2024-11-11 19:25:44 +0530

    Segment tree

--- java/tree/src/Main.java
+++ java/tree/src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 //        BinaryTree tree = new BinaryTree();
@@ -18,15 +16,22 @@ public class Main {
 //        t.insert(9);
 //        t.display();
 //        t.prettyDisplay();
-        AVL t = new AVL();
-        for (int i = 0; i < 50; i++) {
-            t.insert(i);
-        }
+//        AVL t = new AVL();
+//        for (int i = 0; i < 50; i++) {
+//            t.insert(i);
+//        }
+//
+//        System.out.println(t.height());
+//        System.out.println(t.balanced());
+//        System.out.println("=====================================================");
+//        t.prettyDisplay();
 
-        System.out.println(t.height());
-        System.out.println(t.balanced());
-        System.out.println("=====================================================");
-        t.prettyDisplay();
+        int[] arr = {3, 8, 6, 7, -2, -8, 4, 9};
+        Segment tree = new Segment(arr);
+         tree.display();
+
+        System.out.println(tree.query(1, 6));
+
     }
 }
 
